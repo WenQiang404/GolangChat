889840de318763e86916fa5c15aca3c2c1bd1c0f
文强 <120762982+WenQiang404@users.noreply.github.com>
2023-11-17 18:54:51 +0800

    feat: 新增SQL输出日志

--- utils/system_init.go
+++ utils/system_init.go
@@ -4,7 +4,10 @@ import (
 	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"log"
+	"os"
+	"time"
 )
 
 var DB *gorm.DB
@@ -20,7 +23,16 @@ func InitConfig() {
 }
 
 func InitMySQL() {
-	DB, _ = gorm.Open(mysql.Open(viper.GetString("mysql.dns")), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second, //慢SQL阈值
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+	DB, _ = gorm.Open(mysql.Open(viper.GetString("mysql.dns")),
+		&gorm.Config{Logger: newLogger})
 
 	//user := modules.UserBasic{}
 	//DB.Find(&user)
