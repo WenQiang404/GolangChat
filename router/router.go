d7b71205f74c5ef6df6f2c0d336b8f8af2089cdd
文强 <120762982+WenQiang404@users.noreply.github.com>
2023-11-17 19:05:20 +0800

    feat: 更新路由

--- router/router.go
+++ router/router.go
@@ -1,14 +1,23 @@
 package router
 
 import (
+	"GolangChat/docs"
 	"GolangChat/service"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func Router() *gin.Engine {
 	r := gin.Default()
+	docs.SwaggerInfo.BasePath = ""
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	r.GET("/index", service.GetIndex)
-	r.GET("/user", service.GetUser)
 
+	//User相关
+	r.GET("/user/getUser", service.GetUser)
+	r.GET("/user/createUser", service.CreateUser)
+	r.GET("user/deleteUser", service.DeleteUser)
+	r.GET("user/updateUser", service.UpdateUser)
 	return r
 }
