5f70a0d41b88f634d32749b7a3e54190a12f38de
文强 <120762982+WenQiang404@users.noreply.github.com>
2023-11-27 21:38:59 +0800

    feat: 私聊功能实现

--- modules/message_basic.go
+++ modules/message_basic.go
@@ -1,6 +1,8 @@
 package modules
 
 import (
+	log2 "GolangChat/log"
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"gopkg.in/fatih/set.v0"
@@ -14,9 +16,9 @@ import (
 // 消息表
 type MessageBasic struct {
 	gorm.Model
-	FromId   uint   //发送者
-	TargetId uint   //接收者
-	Type     string //消息类型 群聊，私聊
+	UserId   int64  //发送者
+	TargetId int64  //接收者
+	Type     int    //消息类型 群聊，私聊
 	Media    int    //消息类型 文字 图片
 	Content  string //消息内容
 	Pic      string
@@ -38,16 +40,22 @@ type Client struct {
 
 var clientMap map[int64]*Client = make(map[int64]*Client, 0)
 
-var rwMutex = sync.RWMutex{}
+func newLogger() *log2.Logger {
+	return log2.NewLogger()
+}
+
+var log = newLogger()
+
+var rwLocker = sync.RWMutex{}
 
 func Chat(writer http.ResponseWriter, request *http.Request) {
 	query := request.URL.Query()
 	Id := query.Get("userId")
 	userId, _ := strconv.ParseInt(Id, 10, 64)
-	targetId := query.Get("targetId")
-	context := query.Get("context")
-	token := query.Get("token")
-	msgType := query.Get("type")
+	//targetId := query.Get("targetId")
+	//context := query.Get("context")
+	//token := query.Get("token")
+	//msgType := query.Get("type")
 	var isvalid = true //check Token（）
 	conn, err := (&websocket.Upgrader{
 		//token校验
@@ -56,7 +64,7 @@ func Chat(writer http.ResponseWriter, request *http.Request) {
 		},
 	}).Upgrade(writer, request, nil)
 	if err != nil {
-		fmt.Println(err)
+		log.Error("Failed to set the websocket connect! : " + err.Error())
 		return
 	}
 
@@ -69,14 +77,15 @@ func Chat(writer http.ResponseWriter, request *http.Request) {
 
 	//用户关系
 	//id绑定client
-	rwMutex.Lock()
+	rwLocker.Lock()
 	clientMap[userId] = client
-	rwMutex.Unlock()
+	rwLocker.Unlock()
 
 	//发送消息
 	go sendProc(client)
 	//接收消息
 	go receiveProc(client)
+	sendMsg(userId, []byte("Welcome to the lulu chat room !"))
 }
 
 func sendProc(cli *Client) {
@@ -85,7 +94,7 @@ func sendProc(cli *Client) {
 		case data := <-cli.DataQueue:
 			err := cli.Conn.WriteMessage(websocket.TextMessage, data)
 			if err != nil {
-				fmt.Println(err)
+				log.Error("Failed to write message : " + err.Error())
 				return
 			}
 		}
@@ -95,13 +104,15 @@ func receiveProc(cli *Client) {
 	for {
 		_, data, err := cli.Conn.ReadMessage()
 		if err != nil {
-			fmt.Println(err)
+			log.Error("Failed to read message : " + err.Error())
 			return
 		}
 		broadMsg(data)
 		fmt.Println("[ws] >>>>", data)
 	}
 }
+
+var udpsendChan chan []byte = make(chan []byte, 1024)
 
 func broadMsg(data []byte) {
 	udpsendChan <- data
@@ -110,6 +121,7 @@ func broadMsg(data []byte) {
 func init() {
 	go udpSendProc()
 	go udpReceiveProc()
+
 }
 
 // finish the udp send channel
@@ -120,7 +132,8 @@ func udpSendProc() {
 	})
 	defer conn.Close()
 	if err != nil {
-		fmt.Println(err)
+		log.Error("Failed to connect the UDP network : " + err.Error())
+		return
 	}
 
 	for {
@@ -128,9 +141,60 @@ func udpSendProc() {
 		case data := <-udpsendChan:
 			_, err := conn.Write(data)
 			if err != nil {
-				fmt.Println(err)
+				log.Error(err.Error())
 				return
 			}
 		}
+	}
+}
+
+func udpReceiveProc() {
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{
+		IP:   net.IPv4zero,
+		Port: 3000,
+	})
+	if err != nil {
+		log.Error("Failed to connect UDP," + err.Error())
+	}
+	defer conn.Close()
+	for {
+		var buf [512]byte
+		n, err := conn.Read(buf[0:])
+		if err != nil {
+			log.Error("Failed to read buffer, " + err.Error())
+			return
+		}
+		err = dispatch(buf[0:n])
+		if err != nil {
+			log.Error(err.Error())
+			return
+		}
+	}
+}
+
+func dispatch(data []byte) error {
+	msg := MessageBasic{}
+	err := json.Unmarshal(data, &msg)
+	if err != nil {
+		log.Error(err.Error())
+		return err
+	}
+	switch msg.Type {
+	case 1:
+		sendMsg(msg.TargetId, data) //私聊
+		//case 2:
+		//	sendGroup() //群发
+		//case 3:
+		//	sendAll() //广播
+	}
+	return nil
+}
+
+func sendMsg(userId int64, msg []byte) {
+	rwLocker.RLock()
+	node, ok := clientMap[userId]
+	rwLocker.RUnlock()
+	if ok {
+		node.DataQueue <- msg
 	}
 }
