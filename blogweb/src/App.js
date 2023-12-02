2ee5781954f07a4ac35fa53856e56985c00203b2
文强 <120762982+WenQiang404@users.noreply.github.com>
2023-12-02 13:09:58 +0800

    feat:更新页面路由

--- blogweb/src/App.js
+++ blogweb/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignInSide from "./component/signIn/SignInside";
+import Page from "./component/chatRoom/chatRoom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<SignInSide />} />
+                <Route path="/chatroom" element={<Page/>} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
